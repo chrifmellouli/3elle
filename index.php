e70dd40668bfe5bcc9f8eb2fd14ff933760d8845
chrifmellouli <mellouli.chrif@gmail.com>
2020-12-31 03:15:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>3elle</title>
+</head>
+<body>
+<h3>
+Site E-commerce
+</h3>
+<h4>
+Ingénieur développement : Chrif MELLOULI
+</h4>
+<h5>
+Pour le compte de la STE 3ELLE
+</h5><h5>Gérant : HENI SCHOURA</h5>
+</h5>
+<h6>
+<?php
+echo 'Version PHP courante : ' . phpversion();
+?>
+</h6>
+</body>
+</html>
